3ac625640793ff6545db115fcf28802cba0779c4
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2025-08-19 20:50:38 +0530

    Update getCurrentUser.jsx

--- frontend/src/customHooks/getCurrentUser.jsx
+++ frontend/src/customHooks/getCurrentUser.jsx
@@ -13,6 +13,7 @@ const getCurrentUser=()=>{
                 let result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
                 dispatch(setUserData(result.data))
             } catch (error) {
+                dispatch(setUserData(null))
                 console.log(error)
             }
         }
