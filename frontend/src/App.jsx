361cfda0e0180a0b3f755e61a651912b557941b3
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2025-08-19 23:44:28 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Landingpage from './pages/Landingpage.jsx';
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import SignUp from './pages/SignUp.jsx'
